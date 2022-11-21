11bb165d78316c306c319bf83f417748eb2166be
Loginov-Alexander-Sergeevich <94897418+Loginov-Alexander-Sergeevich@users.noreply.github.com>
2022-11-21 08:50:06 +0500

    init commit

---
+++ SferaInternshipWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SferaInternshipWeatherApp
+//
+//  Created by Александр Александров on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
